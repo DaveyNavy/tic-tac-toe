64af877eac67a96a9932cddf8122013b56c93cf7
David Zhu <davidyzhu2@gmail.com>
2024-12-08 15:55:30 -0800

    Allow player to input name, keep track of score

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
+const dialog = document.querySelector("dialog");
+dialog.showModal();
+
+const form = document.querySelector("form");
+let player1 = "Player 1";
+let player2 = "Player 2";
+const player1Name = document.querySelector(".player1-name");
+const player2Name = document.querySelector(".player2-name");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    player1 = formProps["player1"] == ""? player1 : formProps["player1"];
+    player2 = formProps["player2"] == ""? player2 : formProps["player2"];
+
+    player1Name.textContent = player1 + ": " + 0;
+    X.setName(player1);
+    player2Name.textContent = player2 + ": " + 0;
+    O.setName(player2);
+
+    dialog.close();
+})
+
+const startButton = document.querySelector(".start");
+let X = makePlayer('X', player1, player1Name);
+let O = makePlayer('O', player2, player2Name);
+startButton.addEventListener("click", () => {
+    gameBoard.restart();
+    currGame = playGame(X, O);
+    display.renderGameboard();
+})
+
+const resetScoresButton = document.querySelector(".resetScores");
+resetScoresButton.addEventListener("click", () => {
+    X.setScore(0);
+    O.setScore(0);
+})
+
+const results = document.querySelector(".result");
+
 const gameBoard = (function () {
     let board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
 
@@ -16,8 +57,8 @@ const gameBoard = (function () {
             if (board[i][0] != '.' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
                 return board[i][0];
             }
-            if (board[i][0] != '.' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
-                return board[i][0];
+            if (board[0][i] != '.' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return board[0][i];
             }
         }
         if (board[0][0] != '.' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
@@ -38,7 +79,12 @@ const gameBoard = (function () {
         return null;
     }
 
-    return { getBoard, placeMark, win };
+    const restart = () => {
+        board = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
+        results.textContent = "";
+    }
+
+    return { getBoard, placeMark, win, restart };
 })();
 
 const display = (function () {
@@ -53,7 +99,7 @@ const display = (function () {
                 button.setAttribute("data-index-number", currIndex);
                 currIndex++;
                 if (square != '.') {
-                    button.innerText = square;
+                    button.textContent = square;
                 }
                 button.addEventListener("click", () => {
                     currGame.getCurrPlayer().makeMove(Math.floor(button.dataset.indexNumber / 3), button.dataset.indexNumber % 3);
@@ -73,20 +119,43 @@ const display = (function () {
     return { renderGameboard, clear };
 })();
 
-function makePlayer(mark) {
+function makePlayer(mark, name, scoreText) {
     const playerMark = mark;
+    let score = 0;
+    let playerName = name;
+
     const makeMove = (i, j) => {
         if (gameBoard.placeMark(playerMark, i, j)) {
             display.renderGameboard();
-            if (gameBoard.win() != null) {
-                alert(gameBoard.win());
-            }
             currGame.togglePlayer();
         } else {
             alert("Illegal");
         }
+        if (gameBoard.win() != null) {
+            if (gameBoard.win() == 'X') {
+                results.textContent = player1 + " Wins!";
+            } else if (gameBoard.win() == 'O') {
+                results.textContent = player2 + " Wins!";
+            } else {
+                results.textContent = "Tie!";
+            }
+            if (gameBoard.win() != "Tie") {
+                score++;
+                scoreText.textContent = playerName + ": " + score;
+            }
+        }
     }
-    return { playerMark, makeMove };
+
+    const setScore = (newScore) => {
+        score = newScore;
+        scoreText.textContent = playerName + ": " + score;
+    }
+
+    const setName = (newName) => {
+        playerName = newName;
+    }
+
+    return { playerMark, makeMove, setScore, setName };
 }
 
 function playGame(player1, player2) {
@@ -98,7 +167,6 @@ function playGame(player1, player2) {
         } else {
             currPlayer = player1;
         }
-        console.log(currPlayer);
     }
 
     const getCurrPlayer = () => {
@@ -107,8 +175,3 @@ function playGame(player1, player2) {
 
     return { getCurrPlayer, togglePlayer }
 }
-
-let X = makePlayer('X');
-let O = makePlayer('O');
-let currGame = playGame(X, O);
-display.renderGameboard();
